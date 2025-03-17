624d7e03e55ec7d7897edb9227a653f01369b4f7
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-03-17 15:25:23 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -6,3 +6,14 @@ if rev==str:
     print("palindrome")
 else: 
     print("not a palindrome")
+
+
+
+s="sos sos sos"
+print(s.replace(" "," "))
+str=s.upper()
+rev=str[-1: :-1]
+if rev==str:
+    print("palindrome")
+else: 
+    print("not a palindrome")
