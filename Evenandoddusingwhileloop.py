edd438ae53c15d819109cc80e5b25448e02a1d68
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-03-18 15:39:26 +0530

    Create Evenandoddusingwhileloop.py

---
+++ Evenandoddusingwhileloop.py
@@ -0,0 +1,14 @@
+n=0
+while n<=100:
+    if n%2==0:
+        if n%4==0:
+            print(n)
+        else:
+            print("even,but not a multiple of 4")
+    else:
+        if n%3==0:
+            print(n)
+        else:
+            print("odd,but not a multiple of 3")
+    n+=1
+            
