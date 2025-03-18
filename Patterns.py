fd2d15621087a28babf7d78eca040e46b6ae4ffd
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-03-18 16:30:54 +0530

    Create Patterns.py

---
+++ Patterns.py
@@ -0,0 +1,30 @@
+#SOLID SQUARE PATTERNS
+
+n=10
+for i in range(n):
+    print("* " * n)
+
+#SOLID RHOMBUS PATTERN 
+
+n=10
+for i in range(n):
+    print(" "*i + "* " * n)
+
+#HALLOW SQUARE PATTERN
+
+n=10
+for i in range(n):
+    if i==0 or i==(n-1):
+        print("* "*n)
+    else: 
+        print("* " + "  "*(n-2) + "*")
+
+#RIGHT ANGLE TRIANGLE OR RIGHT PYRAMID 
+
+n=10
+for i in range(n):
+    print("* "*i)
+
+#LEFT PYRAMID 
+    
+
