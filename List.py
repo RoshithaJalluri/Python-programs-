a62965cb9a63d109dd4716d594e1f0bf99982e06
RoshithaJalluri <22mq1a4215@sviet.edu.in>
2025-03-25 15:42:58 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,28 @@
+#SUM OF LIST
+lst1=[1,5,8,2]
+sum=0
+for i in lst1:
+    sum+=i
+    print(sum)
+    
+#USING FUNCTIONS DUM OF LIST
+def sumofnum():
+    lst1=[1,5,8,2]
+sum=0
+for i in lst1:
+    sum+=i
+print(sum)
+
+#FACTORIAL
+fact=1
+for i in range(1,6):
+    fact*=i
+print(fact)
+
+#UDING FUNCTIONS FACTORIAL
+def factorial():
+    fact=1
+    for i in range(1,6):
+        fact*=i
+print(fact)
+
